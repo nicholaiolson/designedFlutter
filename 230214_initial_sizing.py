ae03bdde48b0b34f24fb7c3890f3c707da22584c
Nicholai Olson <nicholaikkolson@gmail.com>
2023-02-14 16:39:47 -0800

    Test initial

---
+++ 230214_initial_sizing.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Nicholai Olson
+230214
+
+Purpose: Quick Initial Sizing Study
+"""
+
+#%% INITIAL DESIGN TARGETS
+
+#Wing Area
+S = 
+
+#Stall Speed
+VS = 
+
+#Wing Loading 
+W_S = 
+
+#%% MODE ESTIMATES
+
+
+#%% LOADS & STRESS
+
+
